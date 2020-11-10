46bd0e246392475dbc1bd62da93cc81d6ebff9e9
Brainfuck <sasa_yoshi@docomo.ne.jp>
2020-11-10 19:14:41 +0900

    :tada: 掃き出し法 練習

---
+++ prg01.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+#define N 3
+
+void row_reduction(double a[][N + 1]);
+
+
+int main(void)
+{
+  double a[N][N + 1];
+
+  a[0][0] = 2;  a[0][1] = 2;  a[0][2] =  6;  a[0][3] = 24;
+  a[1][0] = 3;  a[1][1] = 5;  a[1][2] = 13;  a[1][3] = 52;
+  a[2][0] = 5;  a[2][1] = 8;  a[2][2] = 24;  a[2][3] = 93;
+
+  row_reduction(a);
+
+  for (int i = 0; i < N; i++)  printf("x[%d] = %f\n", i, a[i][N]);
+}
+
+
+// 掃き出し法
+void row_reduction(double a[][N + 1])
+{
+  double pivot, t;
+
+  for (int i = 0; i < N; i++) {
+    pivot = a[i][i];
+
+    for (int j = i; j < N + 1; j++)  a[i][j] = a[i][j] / pivot;
+
+    for (int k = 0; k < N; k++){
+      if ((k - i) != 0) {
+        t = a[k][i];
+        for (int j = i; j < N + 1; j++)  a[k][j] = a[k][j] - t * a[i][j];
+      }
+    }
+  }
+}
