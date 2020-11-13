bdba195abd65ee1c8c4d6c57aeb4959441e3fe81
Brainfuck <sasa_yoshi@docomo.ne.jp>
2020-11-13 16:18:43 +0900

    :tada: 最小二乗法

--- prg05.c
+++ prg05.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-#define N 4     // データ個数
+#define N 5     // データ個数
 
 double a[2];    // 切片と傾きを入れるグローバル変数
 
@@ -9,11 +9,10 @@ void least_square(double x[N], double y[N]);
 
 int main(void)
 {
-  double x[N] = {1, 2, 3, 4};
-  double y[N] = {9, 34, 85, 171};
+  double x[N] = {1, 2, 3, 4, 5};
+  double y[N] = {9, 34, 85, 171, 188};
 
   least_square(x, y);
-  printf("y = %lfx + %lfx\n", a[1], a[0]);   // 近似曲線
 }
 
 
@@ -22,6 +21,7 @@ void least_square(double x[N], double y[N])
 {
   double sum_x2 = 0, sum_xy = 0, sum_x = 0, sum_y = 0;
 
+  // それぞれの総和を求める
   for (int i = 0; i < N; i++) {
     sum_x2 += x[i] * x[i];
     sum_xy += x[i] * y[i];
@@ -30,5 +30,7 @@ void least_square(double x[N], double y[N])
   }
 
   a[0] = (sum_x2 * sum_y - sum_x * sum_xy) / (N * sum_x2 - sum_x * sum_x);
-  a[1] = (N * sum_xy - sum_x * sum_y) / (N * sum_x2 - sum_x * sum_x);
+  a[1] = (N * sum_xy - sum_x * sum_y)      / (N * sum_x2 - sum_x * sum_x);
+
+  printf("\n近似式 y = %f + %fx\n", a[0], a[1]);
 }
