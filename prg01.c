f5a51eb4fc1591d450969fa2f25a8a05fa887cf5
Brainfuck <sasa_yoshi@docomo.ne.jp>
2020-11-13 16:15:47 +0900

    :tada: 掃き出し法

--- prg01.c
+++ prg01.c
@@ -1,40 +1,43 @@
 #include <stdio.h>
 
-#define N 3
+#define N 4   // 未知数をマクロ定義
 
-void row_reduction(double a[][N + 1]);
+void gauss_jordan(double a[N][N + 1]);
 
 
 int main(void)
 {
-  double a[N][N + 1];
+  double a[N][N + 1];   // 係数データを初期値として配列に格納
 
-  a[0][0] = 2;  a[0][1] = 2;  a[0][2] =  6;  a[0][3] = 24;
-  a[1][0] = 3;  a[1][1] = 5;  a[1][2] = 13;  a[1][3] = 52;
-  a[2][0] = 5;  a[2][1] = 8;  a[2][2] = 24;  a[2][3] = 93;
+  a[0][0] =  1;  a[0][1] =  2;  a[0][2] = -3;  a[0][3] = 2;  a[0][4] =  3;
+  a[1][0] =  0;  a[1][1] = -3;  a[1][2] =  2;  a[1][3] = 1;  a[1][4] =  7;
+  a[2][0] = -3;  a[2][1] =  4;  a[2][2] =  1;  a[2][3] = 2;  a[2][4] =  9;
+  a[3][0] =  3;  a[3][1] =  2;  a[3][2] = -3;  a[3][3] = 0;  a[3][4] = -1;
 
-  row_reduction(a);
+  gauss_jordan(a);
 }
 
 
-// 掃き出し法
-void row_reduction(double a[][N + 1])
+// ガウス・ジョルダン法（掃き出し法）
+void gauss_jordan(double a[N][N + 1])
 {
-  double pivot, t;
+  double pivot;   // 掃き出しに使う対角成分
 
   for (int i = 0; i < N; i++) {
     pivot = a[i][i];
 
-    for (int j = i; j < N + 1; j++)  a[i][j] = a[i][j] / pivot;
+    for (int j = i; j < N + 1; j++)  a[i][j] /= pivot;
 
     for (int k = 0; k < N; k++){
       if ((k - i) != 0) {
-        t = a[k][i];
+        double t = a[k][i];
 
-        for (int j = i; j < N + 1; j++)  a[k][j] = a[k][j] - t * a[i][j];
+        for (int j = i; j < N + 1; j++)  a[k][j] -= t * a[i][j];
       }
     }
   }
 
-  for (int i = 0; i < N; i++)  printf("x[%d] = %f\n", i, a[i][N]);
+  printf("\n%d元連立方程式\n\n", N);
+
+  for (int i = 0; i < N; i++)  printf("解 x[%d] = %f\n", i, a[i][N]);
 }
