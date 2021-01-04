72bfcce3040122630cca8c63755b6592f4075eff
Brainfuck <sasa_yoshi@docomo.ne.jp>
2021-01-04 23:56:00 +0900

    :tada: モンテカルロ法 練習

--- prg10.c
+++ prg10.c
@@ -9,10 +9,10 @@ int main(void)
   double n = 10000;
   double z = 0;
 
+
   // モンテカルロ法
   for (int i = 1; i <= n; i++) {
     double x = (double)rand() / RAND_MAX;
-    double y = (double)rand() / RAND_MAX;
     z += f(x);
 
     if (i % 1000 == 0)  printf("\n計算結果 %d回目：%f\n", i, z / i);
