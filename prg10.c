de74ca8bdb856b66e4e91fa44a2ce19d4abd9d3b
Brainfuck <sasa_yoshi@docomo.ne.jp>
2021-01-06 15:59:53 +0900

    :tada: モンテカルロ法

--- prg10.c
+++ prg10.c
@@ -2,19 +2,19 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define f(x) (exp(-x))
+#define f(x, y) (x + y)
 
 int main(void)
 {
-  double n = 10000;
+  double n = 100000;
   double z = 0;
-
 
   // モンテカルロ法
   for (int i = 1; i <= n; i++) {
     double x = (double)rand() / RAND_MAX;
-    z += f(x);
+    double y = (double)rand() / RAND_MAX;
+    z += f(x, y);
 
-    if (i % 1000 == 0)  printf("\n計算結果 %d回目：%f\n", i, z / i);
+    if (i % 5000 == 0)  printf("\n計算結果 %7d回目 = %f\n", i, z / i);
   }
 }
