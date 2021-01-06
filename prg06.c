68e34073eb93123c53967ea3bffa10e16ab871f7
Brainfuck <sasa_yoshi@docomo.ne.jp>
2021-01-06 15:58:43 +0900

    :tada: 台形法

--- prg06.c
+++ prg06.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
 #include <math.h>
 
-#define f(x) (exp(-x))
+#define f(x) (1 / x)
 
 int main(void)
 {
-  double a = 0.0;           // 積分区間の下端
-  double b = 1.0;           // 積分区間の上端
-  double n = 1024.0;        // 分割数
-  double h = (b - a) / n;   // 刻み幅
-  double s = 0.0;           // 積分値
-
+  double a = 1;    // 積分区間の下端
+  double b = 2;    // 積分区間の上端
 
   // 台形法
-  for (int i = 1; i < n; i++) {
-    double x = a + h * i;
-    s += 2 * f(x);
+  for (int i = 1; i < 11; i++) {
+    double n = pow(2, i);     // 分割数
+    double h = (b - a) / n;   // 刻み幅
+    double s = 0;             // 積分値
+
+    for (int j = 1; j < n; j++) {
+      double x = a + h * j;
+      s += 2 * f(x);
+    }
+
+    s = 0.5 * (s + f(a) + f(b)) * h;
+    printf("\n計算結果 2の%2d乗 = %f\n", i, s);
   }
 
-  s = 0.5 * (s + f(a) + f(b)) * h;
-
-  printf("\n計算結果 = %f\n", s);
+  return 0;
 }
