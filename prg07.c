f695ee4dc7daf22e3d36825c9be6b589099f3640
Brainfuck <sasa_yoshi@docomo.ne.jp>
2021-01-06 15:58:59 +0900

    :tada: シンプソン法

--- prg07.c
+++ prg07.c
@@ -1,29 +1,34 @@
 #include <stdio.h>
 #include <math.h>
 
-#define f(x) (exp(-x))
+#define f(x) (1 / x)
 
 int main(void)
 {
-  double a = 0.0;                 // 積分区間の下端
-  double b = 1.0;                 // 積分区間の上端
-  double n = 1024.0;              // 分割数
-  double h = (b - a) / (2 * n);   // 刻み幅
-  double s, s1, s2 = 0.0;         // 積分値
-
+  double a = 1;   // 積分区間の下端
+  double b = 2;   // 積分区間の上端
 
   // シンプソン法
-  for (int i = 1; i <= 2 * n - 1; i += 2) {
-    double x = a + h * i;
-    s1 += 4 * f(x);
+  for (int i = 1; i < 11; i++) {
+    double n = pow(2, i);           // 分割数
+    double h = (b - a) / (2 * n);   // 刻み幅
+    double s = 0;                   // 積分値
+
+    double s1 = 0;
+    for (int j = 1; j <= 2 * n - 1; j += 2) {
+      double x = a + h * j;
+      s1 += 4 * f(x);
+    }
+
+    double s2 = 0;
+    for (int j = 2; j <= 2 * n - 2; j += 2) {
+      double x = a + h * j;
+      s2 += 2 * f(x);
+    }
+
+    s = (s1 + s2 + f(a) + f(b)) * h / 3;
+    printf("\n計算結果 2の%2d乗 = %f\n", i, s);
   }
 
-  for (int i = 2; i <= 2 * n - 2; i += 2) {
-    double x = a + h * i;
-    s2 += 2 * f(x);
-  }
-
-  s = (s1 + s2 + f(a) + f(b)) * h / 3;
-
-  printf("\n計算結果 = %f\n", s);
+  return 0;
 }
