b7c79f25f7857ddea18c78142e0bb0d19dfb0cb0
Brainfuck <sasa_yoshi@docomo.ne.jp>
2021-01-06 15:59:21 +0900

    :tada: オイラー法

--- prg08.c
+++ prg08.c
@@ -1,24 +1,29 @@
 #include <stdio.h>
+#include <math.h>
 
-#define f(x, y) (x * y)
+#define f(x, y) (x + y)
 
 int main(void)
 {
-  double a = 0.0;           // 区間a
-  double b = 1.0;           // 区間b
-  double eta = 1.0;         // 初期値 y(a)
-  double n = 1024.0;        // 分割数
-  double h = (b - a) / n;   // 刻み幅
-
+  double a = 0;     // 区間a
+  double b = 2;     // 区間b
+  double eta = 1;   // 初期値 y(a)
 
   // オイラー法
-  double x = a;
-  double y = eta;
+  for (int i = 1; i < 11; i++) {
+    double n = pow(2, i);     // 分割数
+    double h = (b - a) / n;   // 刻み幅
 
-  while (x < b) {
-    y = y + h * f(x, y);
-    x += h;
+    double x = a;
+    double y = eta;
+
+    while (x < b) {
+      y = y + h * f(x, y);
+      x += h;
+    }
+
+    printf("\n計算結果 2の%2d乗 = %f\n", i, y);
   }
 
-  printf("\n計算結果 = %f\n", y);
+  return 0;
 }
